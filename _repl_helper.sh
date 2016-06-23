c4abac70fd4ed368e4a580bad4cb3fb0ced56c0c
Kostyantyn Kovalskyy <kostelkow@gmail.com>
2016-06-22 22:42:50 -0400

    Cleaning up repl.sh and removed helper.

--- _repl_helper.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "var scm = require('./js_std');"
-
-while true; do
-   guile ir.scm 2>/tmp/bad_guile.log
-done
--- repl.sh
+++ repl.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-println "var scm = require('./js_std.js');"
+if [[ $@ == **gen** ]]
+then
+   ###REPL HELPER
+   echo "var scm = require('./js_std');"
+
+   while true; do
+      guile ir.scm 2>/tmp/bad_guile.log
+   done
+   ###END REPL HELPER
+fi
+
+#printf "var scm = require('./js_std.js');"
 
 evu=phantomjs
 #evu="node -i"
 #evu=./scheme-electron/run.py
 #(scm.screenshot "http://github.com" "test.png")
 
-./_repl_helper.sh | $evu
+$0 gen | $evu
