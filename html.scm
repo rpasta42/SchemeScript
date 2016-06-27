133b9b0761cda0faff05e54fb36abfcce300a8de
Kostyantyn Kovalskyy <kostelkow@gmail.com>
2016-06-26 21:41:10 -0400

    Html library.

--- html.scm
+++ html.scm
@@ -11,8 +11,8 @@
              (set-car! lst val))
       (set! lst (list val))));(set-car! lst val
 
-;(list tags attrs styles)
-(define (new-html-obj) (list name (hash-new) (hash-new) (hash-new)))
+;(list name tags attrs styles)
+(define (new-html-obj) (list "" (hash-new) (hash-new) (hash-new)))
 
 (define (ho-get-name ho) (car ho))
 (define (ho-get-tags ho) (cadr ho))
@@ -22,27 +22,44 @@
 (define (ho-set-name ho name) (set-car! ho name))
 (define (ho-add-tag ho tag)     (hash-add (ho-get-tags ho) (ir-get-tmp-name) tag))
 (define (ho-add-attr ho attr)   (hash-add (ho-get-attrs ho) (ir-get-tmp-name) attr))
-(define (ho-add-style ho style) (hash-add (ho-get-styles ho) (itr-get-tmp-name) style))
+(define (ho-add-style ho style) (hash-add (ho-get-styles ho) (ir-get-tmp-name) style))
 
-(define (html-syntax-macro-pair-sym doc exp)
+(define (html-style-gen hobj styles)
+   (if (null? styles)
+      '()
+      (begin (ho-add-style hobj (car styles))
+             (html-style-gen hobj (cdr styles)))))
+
+(define (html-syntax-macro-pair-sym hobj exp)
    (define obj-type (car exp))
    (cond
-      ((eq? obj-type 'tag) (ho-set-name doc (cadr exp)))
-      (;(ho-add-tag doc (cadr exp)))
-      (else (ho-add-style doc "random style"))))
+      ((eq? obj-type 'tag)
+       (begin
+         (ho-set-name hobj (cadr exp))
+         (map (lambda (x) (html-syntax-macro hobj x)) (cddr exp))) ;cddr or caddr??
+      ((eq? obj-type 'style) ;(ho-add-tag hobj (cadr exp)))
+       (cons "style" hobj));kkkk latest (html-style-gen hobj (cdr exp)))
+      (else (display "bad option in html parser")))))
+   ;(ho-add-style hobj "random style"))))
 
-(define (html-syntax-macro doc exp)
+(define (html-syntax-macro hobj exp)
    (cond
-      ((not (pair? exp)) exp)
+      ((not (pair? exp)) (cons 'kkkzz (cons exp hobj)))
       ((symbol? (car exp))
-       (begin (html-syntax-macro-pair-sym doc exp)
-              doc))
-      (else (cons (html-syntax-macro doc (car exp) (html-syntax-macro doc (cdr exp)))))))
+       (begin (html-syntax-macro-pair-sym hobj exp)
+              hobj))
+      (else (cons
+               (html-syntax-macro hobj (car exp))
+               (html-syntax-macro hobj (cdr exp))))))
 
 (define test-exp-pic
    '(define my-pic (tag img (attr src "test.png"))))
 
 (define test-exp
+   '(tag div
+      (style (test a) (test b))))
+
+(define test-exp-real
    '(tag div
       (attr (color red) (id "yo") (class "hi"))
       (style (background-color red) (border-radius 10))
